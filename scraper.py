defb54de7c5b64e7523224c9b9e179072e71d7af
Goldman Park <death2501@gmail.com>
2021-07-21 23:07:50 +0900

    21.07.21 edit
    PAGE_DOWN test(failed)

--- scraper.py
+++ scraper.py
@@ -9,20 +9,21 @@ import time
 import threading
 import urllib.request
 from pathlib import Path
-from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
 
-# Initialize..
+# Initialize global vaiables
 gfMembers = ['소원', '예린', '은하', '유주', '신비', '엄지']
 threadStart = [False, False, False, False, False, False]
 XPATH_IMG = '//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div[1]/a/img'
 chromeDriverLocation = os.getcwd() + '/chromedriver.exe'
 
-def findOrCreateDirectory(name):
+def findOrCreateDirectory(idx):
     try:
+        name = gfMembers[idx]
         Path(os.getcwd() + '/' + name).mkdir(parents=True, exist_ok=True)
     except Exception as e:
         print(e)
@@ -35,35 +36,50 @@ def storeMemberImage(idx):
     chromeDriver.get('https://www.google.co.kr/imghp?hl=ko')
     chromeDriver.find_element_by_xpath('//*[@id="sbtc"]/div/div[2]/input').send_keys('여자친구 ' + name)
     chromeDriver.find_element_by_xpath('//*[@id="sbtc"]/button').click()
-    
-    # wait until browsers in other threads can start
-    threadStart[idx] = True    
-    while not all(threadStart):
-        pass
+    chromeDriver.implicitly_wait(2)
 
-    for i in range(1,20):
+    # wait until browsers in other threads can start
+    # threadStart[idx] = True    
+    # while not all(threadStart):
+    #     pass
+
+    count = 0
+    while(count < 50):
         try:
-            chromeDriver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[' + str(i) + ']/a[1]').click()
-            # WebDriverWait cannot wait until image is fully loaded
-            # chromeWait.until(EC.presence_of_element_located((By.XPATH, XPATH_IMG)))
-            # chromeWait.until(EC.presence_of_element_located((By.CLASS_NAME, 'n3VNCb')))
-            # chromeWait.until(EC.presence_of_element_located((By.TAG_NAME, 'img')))
-            time.sleep(2) # brute but perfect
-            src = chromeDriver.find_element_by_xpath(XPATH_IMG).get_attribute('src')
-            urllib.request.urlretrieve(src, os.getcwd() + '/' + name + '/google_' + format(i, '05') + '.png')
+            # 1. get count of images and gather images
+            currentBodyCount = len(chromeDriver.find_elements_by_css_selector('.rg_i.Q4LuWd'))
+            for i in range(1, currentBodyCount):
+                chromeDriver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[' + str(i) + ']/a[1]').click()
+                # WebDriverWait cannot wait until image is fully loaded
+                # chromeWait.until(EC.presence_of_element_located((By.XPATH, XPATH_IMG)))
+                # chromeWait.until(EC.presence_of_element_located((By.CLASS_NAME, 'n3VNCb')))
+                # chromeWait.until(EC.presence_of_element_located((By.TAG_NAME, 'img')))
+                time.sleep(1.5) # brute but perfect                
+                src = chromeDriver.find_element_by_xpath(XPATH_IMG).get_attribute('src')
+                count += i
+                urllib.request.urlretrieve(src, os.getcwd() + '/' + name + '/google_' + format(count, '05') + '.png')
+                
+            # 2. scroll down
+            chromeDriver.find_element_by_tag_name("body").send_keys(Keys.PAGE_DOWN)
+            time.sleep(1.5)
         except Exception as e:
             print(e)
             pass
 
     chromeDriver.close()
+    print(name + ' finished')
 
-threadList = []
-for idx in range(0, 6):
-    findOrCreateDirectory(gfMembers[idx])
-    th = threading.Thread(target=storeMemberImage, args=(idx,))
-    th.start()
+# Main
+# threadList = []
+# for idx in range(0, 6):
+#     findOrCreateDirectory(idx)
+#     th = threading.Thread(target=storeMemberImage, args=(idx,))
+#     th.start()
 
-for thread in threadList:
-    thread.join()
+# for thread in threadList:
+#     thread.join()
+
+findOrCreateDirectory(0) # 소원 테스트
+storeMemberImage(0)
 
 print('Finished!')
