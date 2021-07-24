af5625a55dfda346f5fcae77b9971443b2b7df76
Goldman Park <death2501@gmail.com>
2021-07-24 22:25:44 +0900

    21.07.24 edit(face detection)
    -how to enhance performance?
    -img source: twitter

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@
 # https://velog.io/@log327/Python-Selenium-Explicit-Waits-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
 
 import os
+import io
 import time
 import threading
 import logging
@@ -12,10 +13,12 @@ import datetime
 import urllib.request
 from pathlib import Path
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
+from mtcnn import MTCNN
+from PIL import Image
+import cv2
 
 # Initialize global vaiables
 gfMembers_KOR = ['소원', '예린', '은하', '유주', '신비', '엄지']
@@ -32,13 +35,14 @@ loggerLock = threading.Lock()
 def findOrCreateDirectory(idx):
     try:
         Path(os.getcwd() + '/' + gfMembers_ENG[idx]).mkdir(parents=True, exist_ok=True)
+        Path(os.getcwd() + '/' + gfMembers_ENG[idx] + '/ORIGINAL').mkdir(parents=True, exist_ok=True)
+        Path(os.getcwd() + '/' + gfMembers_ENG[idx] + '/FACE').mkdir(parents=True, exist_ok=True)
     except Exception as e:
         print(e)
 
-def storeMemberImage(idx, lock):
+def storeMemberImage_Google(idx, lock):
     option = webdriver.ChromeOptions()
-    #option.add_experimental_option('excludeSwitches', ['enable-logging'])
-    option.add_argument('log-level=3')
+    option.add_argument('log-level=3')  # to skip inevitable handshake err log
     chromeDriver = webdriver.Chrome(chromeDriverLocation, options=option)
     chromeDriver.implicitly_wait(3)
     #chromeWait = WebDriverWait(chromeDriver, 10, poll_frequency=1)
@@ -71,14 +75,30 @@ def storeMemberImage(idx, lock):
     currentBodyCount = len(chromeDriver.find_elements_by_css_selector('.rg_i.Q4LuWd'))
     for i in range(1, currentBodyCount):
         try:
+            # click thumbnail to get larger image
             chromeDriver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[' + str(i) + ']/a[1]').click()
             # WebDriverWait cannot wait until image is fully loaded
             # chromeWait.until(EC.presence_of_element_located((By.XPATH, XPATH_IMG)))
             # chromeWait.until(EC.presence_of_element_located((By.CLASS_NAME, 'n3VNCb')))
             # chromeWait.until(EC.presence_of_element_located((By.TAG_NAME, 'img')))
-            time.sleep(1.5) # brute but perfect                
-            src = chromeDriver.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div[1]/a/img').get_attribute('src')
-            urllib.request.urlretrieve(src, os.getcwd() + '/' + gfMembers_ENG[idx] + '/google_' + format(i, '05') + '.png')
+            time.sleep(1.5) # brute but perfect
+
+            # store original image
+            imgUrl = chromeDriver.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div[1]/a/img').get_attribute('src')
+            imgDir = os.getcwd() + '/' + gfMembers_ENG[idx] + '/ORIGINAL/google_' + format(i, '05') + '.png'
+            urllib.request.urlretrieve(imgUrl, imgDir)
+            
+            # face detection and crop, store
+            rgbImg = cv2.cvtColor(cv2.imread(imgDir), cv2.COLOR_BGR2RGB) #numpy array type
+            detector = MTCNN()
+            for result in detector.detect_faces(rgbImg):
+                x = result['box'][0]
+                y = result['box'][1]
+                w = result['box'][2]
+                h = result['box'][3]
+                cropImg = Image.fromarray(rgbImg[y : y + h, x : x + w]) #image
+                cropImg.save(os.getcwd() + '/' + gfMembers_ENG[idx] + '/FACE/google_' + format(i, '05') + '.png')                
+
         except Exception as ex:
             with lock:
                 print(gfMembers_ENG[idx] + '(' + format(i, '05') + ') :')
@@ -94,7 +114,7 @@ def storeMemberImage(idx, lock):
 threadList = []
 for idx in range(0, 6):
     findOrCreateDirectory(idx)
-    th = threading.Thread(target=storeMemberImage, args=[idx, loggerLock], name='thread_' + gfMembers_ENG[idx])
+    th = threading.Thread(target=storeMemberImage_Google, args=[idx, loggerLock], name='thread_' + gfMembers_ENG[idx])
     th.start()
 
 for thread in threadList:
